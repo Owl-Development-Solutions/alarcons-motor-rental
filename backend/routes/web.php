edd32b57614cf5b05c26c9ffdb1e240ac021853d
Jabame731 <justinetaglucop@gmail.com>
2026-07-10 01:33:23 +0800

    removing some files to make purely laravel API

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,4 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'welcome')->name('home');
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'Hello from Laravel API!',
+    ]);
+});
