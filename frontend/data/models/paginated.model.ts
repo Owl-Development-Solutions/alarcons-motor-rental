74e6f28dd9d11491ee4b5859c76a9b7a056e04a1
Jabame731 <justinetaglucop@gmail.com>
2026-07-24 18:16:30 +0800

    fixing the error build

--- frontend/data/models/paginated.model.ts
+++ frontend/data/models/paginated.model.ts
@@ -1,3 +1,18 @@
+export interface Paginated {
+  first_page_url: string;
+  from: number;
+  last_page: number;
+  last_page_url: string;
+  next_page_url: string | null;
+  path: string;
+  per_page: number;
+  prev_page_url: string | null;
+  to: number;
+  total: number;
+  current_page: number;
+  links: PaginatedReponseLinks;
+}
+
 export interface PaginatedResponse<T> {
   data: T[];
   current_page: number;
@@ -17,7 +32,6 @@ export interface PaginatedResponse<T> {
     active: boolean;
   }[];
 }
-
 export interface PaginatedReponseLinks {
   url: string;
   label: string;
